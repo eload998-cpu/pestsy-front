9fba5c07387470d0251c637cdd93f360cfde3769
eload998-cpu <eload998@gmail.com>
2025-10-22 16:59:21 -0400

    Force AGP 8.3.x for Android build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,25 +5,7 @@ const config: CapacitorConfig = {
   appName: 'Pestsy',
   webDir: 'dist',
   server: {
-    androidScheme: 'https'
-  },
-  plugins: {
-    CapacitorSocialLogin: {
-      google: {
-        scopes: ['profile', 'email'],
-        webClientId:
-          '547267894981-b130sbc443qmrj9murll50kq3i13ep3c.apps.googleusercontent.com',
-        androidClientId:
-          '547267894981-b130sbc443qmrj9murll50kq3i13ep3c.apps.googleusercontent.com',
-        iosClientId:
-          '547267894981-b130sbc443qmrj9murll50kq3i13ep3c.apps.googleusercontent.com',
-        serverClientId:
-          '547267894981-b130sbc443qmrj9murll50kq3i13ep3c.apps.googleusercontent.com',
-        clientId:
-          '547267894981-b130sbc443qmrj9murll50kq3i13ep3c.apps.googleusercontent.com',
-        forceCodeForRefreshToken: true,
-      },
-    },
+    androidScheme: 'https',
   },
 };
 
