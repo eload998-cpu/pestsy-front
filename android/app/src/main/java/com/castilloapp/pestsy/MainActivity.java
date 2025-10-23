0117813dc5aad17af623457f21f592c296061a89
felcast <felipe.castillo@epayco.com>
2025-10-23 17:03:14 -0400

    fixes

--- android/app/src/main/java/com/castilloapp/pestsy/MainActivity.java
+++ android/app/src/main/java/com/castilloapp/pestsy/MainActivity.java
@@ -1,5 +1,39 @@
 package com.castilloapp.pestsy;
 
-import com.getcapacitor.BridgeActivity;
+import android.content.Intent;
+import android.util.Log;
 
-public class MainActivity extends BridgeActivity {}
+import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginHandle;
+
+import ee.forgr.capacitor.social.login.GoogleProvider;
+import ee.forgr.capacitor.social.login.SocialLoginPlugin;
+import ee.forgr.capacitor.social.login.ModifiedMainActivityForSocialLoginPlugin;
+
+public class MainActivity extends BridgeActivity implements ModifiedMainActivityForSocialLoginPlugin {
+
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+
+    if (requestCode >= GoogleProvider.REQUEST_AUTHORIZE_GOOGLE_MIN &&
+        requestCode <  GoogleProvider.REQUEST_AUTHORIZE_GOOGLE_MAX) {
+
+      PluginHandle handle = getBridge().getPlugin("SocialLogin");
+      if (handle == null) {
+        Log.i("Google Activity Result", "SocialLogin handle is null");
+        return;
+      }
+      Plugin plugin = handle.getInstance();
+      if (!(plugin instanceof SocialLoginPlugin)) {
+        Log.i("Google Activity Result", "Plugin instance is not SocialLoginPlugin");
+        return;
+      }
+      ((SocialLoginPlugin) plugin).handleGoogleLoginIntent(requestCode, data);
+    }
+  }
+
+  @Override
+  public void IHaveModifiedTheMainActivityForTheUseWithSocialLoginPlugin() {}
+}
